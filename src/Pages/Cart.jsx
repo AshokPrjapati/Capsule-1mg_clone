1bcb7f39f9a4282abb0f82fd8c34cfdae329a3c5
Ashok Kumar <ap271449@gmail.com>
2023-03-01 19:13:17 +0530

    fixed : cart product remove functionality

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { CartContext } from "../Contexts/CartContext";
 import { useContext } from "react";
-import { Box, Container, Flex, Stack } from "@chakra-ui/react";
+import { Box, Container, Stack } from "@chakra-ui/react";
 import CartCard from "../Components/Cart/CartCard";
 import EmptyCartPage from "../Components/Cart/EmptyCartPage";
 import CartSummary from "../Components/Cart/CartSummary";
 
 function Cart() {
-  const { cartProduct, removeCartItem, isCoupanApplied } = useContext(CartContext);
+  const { cartProduct, isCoupanApplied } = useContext(CartContext);
   const [totalPrice, setTotalprice] = useState(0);
 
   let price = 0;
@@ -23,18 +23,13 @@ function Cart() {
     setTotalprice(price.toFixed(2));
   }, [price]);
 
-  const handleRemove = (id) => {
-    removeCartItem(id);
-  };
-
-
   return (
     <Box w="100%" h="100vh" bg={"#f1f4f6"} >
       {
         cartProduct.length ? <Container maxW="6xl" p={"20px"} m="20px auto" display="flex" flexDir={{ base: "column", md: "row" }} gap={4}>
           <Stack w={{ base: "100%", md: "60%" }} gap="10px">
             {cartProduct.map((p) => (
-              <CartCard key={p.id} product={p} handleRemove={handleRemove} />
+              <CartCard key={p.id} product={p} />
             ))}
           </Stack>
           <Box w={{ base: "100%", md: "40%" }}>
